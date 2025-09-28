8b3075106bd4760e3071b725efdc613d9f77f904
Sam <samuel.s.mamane@gmail.com>
2025-09-28 15:20:53 +0000

    test

---
+++ apps/web/src/app/_components/whatsapp-null.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+export const WhatsappNull = () => {
+    const [messages, setMessages] = useState<string[]>([])
+    const [input, setInput] = useState('')
+
+
+    return (
+        <div>
+            {
+                messages.map((message) => (
+                    <div>
+                        {message}
+                    </div>
+                ))
+            }
+
+            <input 
+                type="text"
+                onChange={(event) => {
+                    setInput(event.currentTarget.value)
+                }}
+                value={input}
+            />
+            <input type="button" onClick={() => {
+                setMessages([...messages, input])
+            }} />
+        </div>
+    )
+
+}
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,6 @@
 import { ClientComponent } from './_components/ClientComponent';
 import { ServerComponent } from './_components/ServerComponent';
+import { WhatsappNull } from './_components/whatsapp-null';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <div className="grid gap-8 md:grid-cols-2">
         <ServerComponent />
         <ClientComponent />
+        <WhatsappNull />
       </div>
     </div>
   );
